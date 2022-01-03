900ac17e1da0e3d71d21ca928c7028ccd368a114
René Tom <re.tom2003@gmail.com>
2022-01-03 23:30:38 +0100

    getIntersection part 1

--- src/Catan/Plateau.java
+++ src/Catan/Plateau.java
@@ -401,5 +401,20 @@ public class Plateau {
             System.out.println(c);
         }
     }
+    public Intersection getIntersection(int x,int y) {
+        if( x <= 0 || x > cases.length || y <= 0 || y > cases.length) {
+            System.out.println("Erreur coordonnées de l'intersection");
+            return null;
+        }
+        if ( x == cases.length ) {
+            if ( y < cases.length) {
+                return getCase(x-1,y).getHD();
+            }
+            else {
+
+            }
+        }
+    }
+    
     
 }
